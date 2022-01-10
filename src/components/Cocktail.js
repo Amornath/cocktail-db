5646c8f28a3e9631d5878141ed2c10ab9e06e7b1
Amornath <amor2017nstucste@gmail.com>
2022-01-11 00:06:49 +0600

    card design changes

--- src/components/Cocktail.js
+++ src/components/Cocktail.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 export default function Cocktail({ image, name, id, info, glass }) {
   return (
+    <Link to={`/cocktail/${id}`} className=''>
     <article className='cocktail'>
       <div className='img-container'>
         <img src={image} alt={name} />
@@ -10,10 +11,8 @@ export default function Cocktail({ image, name, id, info, glass }) {
         <h3>{name}</h3>
         <h4>{glass}</h4>
         <p>{info}</p>
-        <Link to={`/cocktail/${id}`} className='btn btn-primary btn-details'>
-          details
-        </Link>
       </div>
     </article>
+    </Link>
   )
 }
