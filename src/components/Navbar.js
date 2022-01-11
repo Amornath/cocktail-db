35c1cbcb60383049b0f39479738d241d45508246
Amornath <amor2017nstucste@gmail.com>
2022-01-11 22:33:36 +0600

    code prettier added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,19 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <nav className='navbar'>
-      <div className='nav-center'>
-        
-        <ul className='nav-links'>
+    <nav className="navbar">
+      <div className="nav-center">
+        <ul className="nav-links">
           <li>
-            <Link to='/'>home</Link>
+            <Link to="/">home</Link>
           </li>
           <li>
-            <Link to='/about'>about</Link>
+            <Link to="/about">about</Link>
           </li>
         </ul>
-        
       </div>
     </nav>
-  )
+  );
 }
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,33 +1,33 @@
-import React from 'react'
-import { useGlobalContext } from '../context'
+import React from "react";
+import { useGlobalContext } from "../context";
 export default function SearchForm() {
-  const { setSearchTerm } = useGlobalContext()
-  const searchValue = React.useRef('')
+  const { setSearchTerm } = useGlobalContext();
+  const searchValue = React.useRef("");
 
   React.useEffect(() => {
-    searchValue.current.focus()
-  }, [])
+    searchValue.current.focus();
+  }, []);
 
   function searchCocktail() {
-    setSearchTerm(searchValue.current.value)
+    setSearchTerm(searchValue.current.value);
   }
   function handleSubmit(e) {
-    e.preventDefault()
+    e.preventDefault();
   }
   return (
-    <section className=''>
-      <form className='' onSubmit={handleSubmit}>
-        <div className='form-control' align="center">
-          <label htmlFor='name'>search your favorite cocktail</label>
+    <section className="">
+      <form className="" onSubmit={handleSubmit}>
+        <div className="form-control" align="center">
+          <label htmlFor="name">search your favorite cocktail</label>
           <input
-            type='text'
-            name='name'
-            id='name'
+            type="text"
+            name="name"
+            id="name"
             ref={searchValue}
             onChange={searchCocktail}
           />
         </div>
       </form>
     </section>
-  )
+  );
 }
